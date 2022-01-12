04babf885881d543c6eca2c1b01700c8eb0ea825
Xun <90738788+xun0101@users.noreply.github.com>
2022-01-12 16:22:41 +0800

    20220112

--- js/all.js
+++ js/all.js
@@ -8,6 +8,80 @@ $('#reg_btn, #login_btn').on('click',function(){
   })
 })
 
+// section03生物種族
+$('#race a').on('click', function () {
+  $('#race a').removeClass('active')
+  $(this).addClass('active')
+})
+
+// Swiper_____________________________________
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  speed: 1000,
+  spaceBetween: 15,
+  centeredSlides: true,
+  autoplay: {
+    delay: 10000
+  },
+  slidesPerView: 'auto',
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    920: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+})
+
 // GSAP______________________________________
 // 註冊
 gsap.registerPlugin(ScrollTrigger,ScrollToPlugin,SplitText);
+
+$('#navbar .main-link').each(function(index){
+  $(this).on('click', function(e){
+    e.preventDefault();//阻止a連結預設動作
+    if($(this).attr('href')==='#section04' || $(this).attr('href')==='#section05'){
+      gsap.to($(window),{
+    scrollTo: {
+      y:`#section0${index+1}`,
+    },
+    duration:1.5,
+    ease: 'back.inOut'
+  })
+    }else{
+      gsap.to($(window),{
+        scrollTo: {
+          y:`#section0${index+1}`,
+          offsetY:150
+        },
+        duration:1.5,
+        ease: 'back.inOut'
+      })
+    }
+    
+  })
+})
